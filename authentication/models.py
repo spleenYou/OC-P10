c375a652023930b40f542dfa5ccebef6bccec164
Anthony YOU <spleen85@proton.me>
2025-03-17 17:27:49 +0100

    Migrate

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    birthday = models.DateTimeField()
+    can_be_contacted = models.BooleanField()
+    can_data_be_shared = models.BooleanField()
+
+    class Meta:
+        verbose_name = 'Liste d\'utilisateur'
+        verbose_name_plural = 'Liste des utilisateurs'
