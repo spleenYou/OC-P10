6debdf5675ec13f2f0ad016a10da94b740b432cb
Anthony YOU <spleen85@proton.me>
2025-03-17 17:59:32 +0100

    Add fields when create an user (bithday, can be contacted and can_data_be_shared)

--- authentication/models.py
+++ authentication/models.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from datetime import datetime
 
 
 class User(AbstractUser):
 
-    birthday = models.DateTimeField()
-    can_be_contacted = models.BooleanField()
-    can_data_be_shared = models.BooleanField()
+    birthday = models.DateField(null=False, blank=False, default=datetime.today().strftime("%Y-%m-%d"))
+    can_be_contacted = models.BooleanField(null=False, blank=False, default=False)
+    can_data_be_shared = models.BooleanField(null=False, blank=False, default=False)
+
+    REQUIRED_FIELDS = ['birthday', 'can_be_contacted', 'can_data_be_shared']
 
     class Meta:
         verbose_name = 'Liste d\'utilisateur'
