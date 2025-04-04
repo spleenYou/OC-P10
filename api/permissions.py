472188478d0cb2487b5de17648c3ed403acf053f
Anthony YOU <spleen85@proton.me>
2025-04-04 16:21:29 +0200

    put permissions in separated file

---
+++ api/permissions.py
@@ -0,0 +1,50 @@
+from rest_framework.permissions import BasePermission
+from api.models import Issue, Contributor, Project
+
+
+class IsAuthor(BasePermission):
+    message = "Vous devez être l'auteur"
+
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.author
+
+
+class IsContributor(BasePermission):
+
+    message = 'Vous ne faites pas partie du projet'
+
+    def has_permission(self, request, view):
+        if view.action == 'create':
+            if 'issue' in request.data:
+                # Tests for project
+                if Issue.objects.filter(pk=request.data['issue']).exists():
+                    project = Issue.objects.get(pk=request.data['issue']).project
+                else:
+                    self.message = 'Création impossible'
+                    return False
+            elif 'project' in request.data:
+                # Tests for issue
+                if 'assigned_user' in request.data:
+                    if not Contributor.objects.filter(
+                        user=request.data['assigned_user'],
+                            project=request.data['project']).exists():
+                        self.message = 'Création impossible'
+                        return False
+                project = Project.objects.get(pk=request.data['project'])
+            return Contributor.objects.filter(project=project, user=request.user).exists()
+        return True
+
+    def has_object_permission(self, request, view, obj):
+        if 'project' in request.path:
+            project = obj
+        else:
+            project = obj.project
+        return Contributor.objects.filter(project=project, user=request.user).exists()
+
+
+class IsNotAllowedToList(BasePermission):
+
+    message = 'Impossible de lister'
+
+    def has_permission(self, request, view):
+        return view.action != 'list'
--- api/views.py
+++ api/views.py
@@ -8,57 +8,10 @@ from api.serializers import (
     IssueDetailSerializer,
     CommentSerializer,
 )
-from rest_framework.permissions import BasePermission, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
-
-
-class IsAuthor(BasePermission):
-    message = "Vous devez être l'auteur"
-
-    def has_object_permission(self, request, view, obj):
-        return request.user == obj.author
-
-
-class IsContributor(BasePermission):
-
-    message = 'Vous ne faites pas partie du projet'
-
-    def has_permission(self, request, view):
-        if view.action == 'create':
-            if 'issue' in request.data:
-                # Tests for project
-                if Issue.objects.filter(pk=request.data['issue']).exists():
-                    project = Issue.objects.get(pk=request.data['issue']).project
-                else:
-                    self.message = 'Création impossible'
-                    return False
-            elif 'project' in request.data:
-                # Tests for issue
-                if 'assigned_user' in request.data:
-                    if not Contributor.objects.filter(
-                        user=request.data['assigned_user'],
-                            project=request.data['project']).exists():
-                        self.message = 'Création impossible'
-                        return False
-                project = Project.objects.get(pk=request.data['project'])
-            return Contributor.objects.filter(project=project, user=request.user).exists()
-        return True
-
-    def has_object_permission(self, request, view, obj):
-        if isinstance(view, ProjectViewset):
-            project = obj
-        else:
-            project = obj.project
-        return Contributor.objects.filter(project=project, user=request.user).exists()
-
-
-class IsNotAllowedToList(BasePermission):
-
-    message = 'Impossible de lister'
-
-    def has_permission(self, request, view):
-        return view.action != 'list'
+from api.permissions import IsAuthor, IsContributor, IsNotAllowedToList
 
 
 class ProjectViewset(ModelViewSet):
